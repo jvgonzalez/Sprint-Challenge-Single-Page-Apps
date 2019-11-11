edef88ff2c29ffbe46e2eced9595edc542c663dc
jvgonzalez <jgonzalezmn@gmail.com>
2019-11-10 20:02:38 -0500

    Rendered characters list and set up routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,80 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "./components/Header.js";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import CharacterLoop from "./components/CharacterList.js";
+import SearchForm from "./components/SearchForm";
+import WelcomePage from "./components/WelcomePage";
 
 
-export default function App() {
-  return (
-    <main>
-      <Header />
-    </main>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      rickmorty: []
+    };
+  }
+
+  componentDidMount() {
+    this.getCharacters("https://rickandmortyapi.com/api/character/")
+  }
+
+  getCharacters = URL => {
+    fetch(URL)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({ rickmorty: data.results });
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+  };
+
+  render() {
+    return(
+      <div>
+        <Header />
+
+        
+
+        <Route path="/characters"
+        render= {props => <CharacterLoop {...props} characterInfo={this.state} />}
+        />
+
+        <Route exact path="/" component={WelcomePage} />
+
+        <Link
+              style={{
+                
+                color: "green",
+                marginLeft: "40%",
+                fontSize: "2em",
+                textAlign: "center",
+                
+                
+                
+              }}
+              to="/characters"
+            >
+              {" "}
+             Get to know the characters
+        </Link>
+
+        <SearchForm />
+
+
+
+
+
+
+        
+
+
+      </div>
+    )
+  }
 }
+
+export default App;
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -5,11 +5,36 @@ export default function WelcomePage() {
     <section className="welcome-page">
       <header>
         <h1>Welcome to the ultimate fan site!</h1>
+
+        <div className="PicContainer">
         <img
           className="main-img"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
         />
+        <img 
+          className="main-img"
+          src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+          alt="morty"
+          />
+              <img 
+          className="main-img"
+          src="https://rickandmortyapi.com/api/character/avatar/3.jpeg"
+          alt="summer"
+          />
+              <img 
+          className="main-img"
+          src="https://rickandmortyapi.com/api/character/avatar/4.jpeg"
+          alt="beth"
+          />
+              <img 
+          className="main-img"
+          src="https://rickandmortyapi.com/api/character/avatar/5.jpeg"
+          alt="jerry"
+          />
+          </div>
+      
+          
       </header>
     </section>
   );
